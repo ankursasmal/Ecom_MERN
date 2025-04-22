3f75f87ff2032b4f59ab9f1c740ceaf44b5c30ab
ankursasmal <153544988+ankursasmal@users.noreply.github.com>
2025-04-22 14:45:17 +0000

    complete new section cart

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -3,11 +3,10 @@ let mongoose=require('mongoose')
 // connect with driver
 async function connectionDB (){
    
-    // MONGO_DRIVE_DB='mongodb+srv://ankurEcommerse:Ankur123@cluster0.sgwwiml.mongodb.net/Ecommerse?retryWrites=true&w=majority&appName=Cluster0'
-    // SECRET_KEY='3y2yxhx829299292hc2rhh9h2rhcj9j2rj9r9rj92' 
+    
 
     try{
-mongoose.connect('mongodb+srv://ankurEcommerse:Ankur123@cluster0.sgwwiml.mongodb.net/Ecommerse?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect(process.env.MongoConnection);
      console.log('db connect');
 }
 catch(e){
